5998bc0ffc23ac31dd6b115acb4ba9b6323c39d0
rla9788 <121483272+rla9788@users.noreply.github.com>
2024-04-30 02:04:01 +0900

    Delete 30903 directory

--- 30903/script/script.js
+++
@@ -1,6 +0,0 @@
-$('.menu > li').mouseover(function(){
-    $(this).children('.submenu').stop().slideDown(200);
-});
-$('.menu > li').mouseout(function(){
-    $(this).children('.submenu').stop().slideUp(200);
-});
