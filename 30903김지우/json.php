6f214d06c9512281946b0e6a066f402d650ffaef
rla9788 <121483272+rla9788@users.noreply.github.com>
2024-10-16 10:49:51 +0900

    Update json.php

--- 30903김지우/json.php
+++ 30903김지우/json.php
@@ -1,7 +1,29 @@
 <?php
-    header("Content-Type: application/json; charset=utf-8");
+    header(header: "Content-Type: application/json; charset=utf-8");
 
     $name = $_POST['name'];
     $email = $_POST['email'];
-    echo(json_encode(array("name" => $name, "email" => $email)));
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "webcontent";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sql = "INSERT INTO member (name, email)
+    VALUES ('$name', '$email')";
+
+    if (mysqli_query($conn, $sql)) {
+    echo "성공";
+    } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
 ?>
